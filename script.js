a6db0c080e46b84c5e9f959edcc6b96a1925008d
user.name like:"compaoreh338 <user.email like"compaoreh338@gmail.com>
2023-05-29 12:03:17 +0000

    modification des codes

--- script.js
+++ script.js
@@ -1,4 +1,33 @@
-function modal(){
-    el = document.getElementById("modal");
-    el.style.visibility = (el.style.visibility == "visible") ? "hidden" : "visible";
+// Ouvrir la page modale en cliquant sur le bouton
+var openModalButton = document.getElementById("openModal");
+var modal = document.getElementById("myModal");
+
+openModalButton.addEventListener("click", function () {
+    modal.style.display = "block";
+});
+
+// Fermer la page modale en cliquant sur le bouton de fermeture
+var closeButton = document.getElementsByClassName("close")[0];
+
+closeButton.addEventListener("click", function () {
+    confirmCloseModal();
+});
+
+// Fermer la page modale en cliquant en dehors de celle-ci
+window.addEventListener("click", function (event) {
+    if (event.target == modal) {
+        confirmCloseModal();
+    }
+});
+
+// Fonction pour confirmer la fermeture de la page modale
+function confirmCloseModal() {
+    if (confirm("Êtes-vous sûr de vouloir fermer la page modale ?")) {
+        closeModal();
+    }
+}
+
+// Fonction pour fermer la page modale
+function closeModal() {
+    modal.style.display = "none";
 }
